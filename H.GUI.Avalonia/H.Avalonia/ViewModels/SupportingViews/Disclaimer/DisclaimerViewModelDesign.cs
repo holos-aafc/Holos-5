5bf8d39625a186a77d4b8b45511bc640858441a6
holos-aafc <95769272+holos-aafc@users.noreply.github.com>
2024-08-27 13:44:02 -0600

    Add design view model so that view can get context for auto complete:
    https://docs.avaloniaui.net/docs/guides/implementation-guides/how-to-use-design-time-data

---
+++ H.GUI.Avalonia/H.Avalonia/ViewModels/SupportingViews/Disclaimer/DisclaimerViewModelDesign.cs
@@ -0,0 +1,8 @@
+﻿namespace H.Avalonia.ViewModels.SupportingViews.Disclaimer;
+
+public class DisclaimerViewModelDesign : DisclaimerViewModel
+{
+    public DisclaimerViewModelDesign()
+    {
+    }
+}

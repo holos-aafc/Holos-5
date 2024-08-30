c4865fbd4b9fcba2bf9c3e268cdc201fc7ac3936
holos-aafc <95769272+holos-aafc@users.noreply.github.com>
2024-08-30 11:53:45 -0600

    Add settings file to indicate which version of system should be run. Use dependecy injection to give classes an object to chech which setting is used

---
+++ H.Core/Helpers/ConfigurationFileHelper.cs
@@ -0,0 +1,64 @@
+﻿using Splat;
+using System.Configuration;
+using H.Core.Enumerations;
+
+namespace H.Core.Helpers;
+
+public class ConfigurationFileHelper
+{
+    #region Fields
+
+    #endregion
+
+    #region Public Methods
+
+    public static bool BooleanAppSettingIsEnabled(string settingName)
+    {
+        var result = false;
+
+        try
+        {
+            ConfigurationManager.RefreshSection("appSettings");
+
+            var config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            if (config.AppSettings.Settings.AllKeys.Contains(settingName))
+            {
+                var setting = config.AppSettings.Settings[settingName].Value;
+                result = setting.Equals("true", StringComparison.InvariantCultureIgnoreCase) || setting.Equals("t", StringComparison.InvariantCultureIgnoreCase) || setting.Equals("1", StringComparison.InvariantCultureIgnoreCase);
+            }
+        }
+        catch (Exception e)
+        {
+        }
+
+        return result;
+    }
+
+    public static CountryVersion GetCountryVersion()
+    {
+        CountryVersion countryVersion = CountryVersion.Canada;
+        const string settingName = "CountryVersion";
+
+        try
+        {
+            ConfigurationManager.RefreshSection("appSettings");
+
+            var config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            if (config.AppSettings.Settings.AllKeys.Contains(settingName))
+            {
+                var setting = config.AppSettings.Settings[settingName].Value;
+                if (setting.Equals("ireland", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    countryVersion = CountryVersion.Ireland;
+                }
+            }
+        }
+        catch (Exception e)
+        {
+        }
+
+        return countryVersion;
+    }
+
+    #endregion
+}
--- H.GUI.Avalonia/H.Avalonia/ViewModels/SupportingViews/Disclaimer/DisclaimerViewModel.cs
+++ H.GUI.Avalonia/H.Avalonia/ViewModels/SupportingViews/Disclaimer/DisclaimerViewModel.cs
@@ -12,6 +12,7 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using H.Avalonia.Views;
+using H.Core.Services;
 using Prism.Commands;
 
 namespace H.Avalonia.ViewModels.SupportingViews.Disclaimer
@@ -31,6 +32,8 @@ namespace H.Avalonia.ViewModels.SupportingViews.Disclaimer
 
         private DelegateCommand<object> _okCommand;
 
+        private ICountrySettings _countrySettings;
+
         #endregion
 
         #region Constructors
@@ -42,8 +45,18 @@ namespace H.Avalonia.ViewModels.SupportingViews.Disclaimer
 
         public DisclaimerViewModel(IRegionManager regionManager,
                                    IEventAggregator eventAggregator,
-                                   Storage storage) : base(regionManager, eventAggregator, storage)
+                                   Storage storage,
+                                   ICountrySettings countrySettings) : base(regionManager, eventAggregator, storage)
         {
+            if (countrySettings != null)
+            {
+                _countrySettings = countrySettings; 
+            }
+            else
+            {
+                throw new ArgumentNullException(nameof(countrySettings));
+            }
+
             LanguageCollection = new ObservableCollection<Languages>(EnumHelper.GetValues<Languages>());
             this.Construct();
         }

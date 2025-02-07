d372c84aef457a0ff5e830207860cbab7e91efa3
raheemmir <raheem.mir@agr.gc.ca>
2025-02-07 11:14:49 -0700

    Added some binding between horse view / view model + created horse view model design file

--- H.GUI.Avalonia/H.Avalonia/ViewModels/ComponentViews/OtherAnimals/HorsesComponentViewModel.cs
+++ H.GUI.Avalonia/H.Avalonia/ViewModels/ComponentViews/OtherAnimals/HorsesComponentViewModel.cs
@@ -1,7 +1,30 @@
-﻿namespace H.Avalonia.ViewModels.ComponentViews.OtherAnimals
+﻿using System;
+using System.Collections.ObjectModel;
+using H.Core.Models.Animals;
+using Prism.Regions;
+
+namespace H.Avalonia.ViewModels.ComponentViews.OtherAnimals
 {
     public class HorsesComponentViewModel : ViewModelBase
     {
+        public string ViewName { get; set; }
 
+        public ObservableCollection<ManagementPeriod> ManagementPeriods { get; set; }
+
+        public HorsesComponentViewModel() 
+        {
+            ViewName = "Horses";
+
+            ManagementPeriods = new ObservableCollection<ManagementPeriod>
+            {
+                new ManagementPeriod {GroupName = "Period #1", Start=new DateTime(2020,03,13), End= new DateTime(2021,03,13), NumberOfDays=364},
+                new ManagementPeriod {GroupName = "Period #2", Start=new DateTime(2022,12,12), End= new DateTime(2023,12,12), NumberOfDays=364}
+            };
+        }
+
+        public override void OnNavigatedTo(NavigationContext navigationContext)
+        {
+            base.OnNavigatedTo(navigationContext);
+        }
     }
 }
---
+++ H.GUI.Avalonia/H.Avalonia/ViewModels/ComponentViews/OtherAnimals/HorsesComponentViewModelDesign.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using H.Core.Models.Animals;
+
+namespace H.Avalonia.ViewModels.ComponentViews.OtherAnimals
+{
+    public class HorsesComponentViewModelDesign : HorsesComponentViewModel
+    {
+        public HorsesComponentViewModelDesign() 
+        {
+            base.ViewName = "Horses";
+            base.ManagementPeriods.Add(new ManagementPeriod { GroupName = "Test Group #1", Start=new DateTime(2000,01,01), End=new DateTime(2001,01,01), NumberOfDays=364});
+        }
+    }
+}

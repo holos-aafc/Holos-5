5388b26ef8480a12b66c66c0ea7b1e7231de66b2
raheemmir <raheemmir15@gmail.com>
2025-02-07 15:42:03 -0700

    Factored out steps 1 and 2 sections of horse view to be used by all other animal views

--- H.GUI.Avalonia/H.Avalonia/ViewModels/ComponentViews/OtherAnimals/HorsesComponentViewModel.cs
+++ H.GUI.Avalonia/H.Avalonia/ViewModels/ComponentViews/OtherAnimals/HorsesComponentViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.ObjectModel;
 using H.Core.Models.Animals;
+using H.Core.Enumerations;
 using Prism.Regions;
 
 namespace H.Avalonia.ViewModels.ComponentViews.OtherAnimals
@@ -10,15 +11,23 @@ namespace H.Avalonia.ViewModels.ComponentViews.OtherAnimals
         public string ViewName { get; set; }
 
         public ObservableCollection<ManagementPeriod> ManagementPeriods { get; set; }
+        public ObservableCollection<AnimalGroup> Groups { get; set; }
 
         public HorsesComponentViewModel() 
         {
             ViewName = "Horses";
+            
+            // Populating some dummy data (i.e. for testing DataGrids)
+
+            Groups = new ObservableCollection<AnimalGroup>
+            {
+                new AnimalGroup {GroupType = AnimalType.Horses}
+            };
 
             ManagementPeriods = new ObservableCollection<ManagementPeriod>
             {
-                new ManagementPeriod {GroupName = "Period #1", Start=new DateTime(2020,03,13), End= new DateTime(2021,03,13), NumberOfDays=364},
-                new ManagementPeriod {GroupName = "Period #2", Start=new DateTime(2022,12,12), End= new DateTime(2023,12,12), NumberOfDays=364}
+                new ManagementPeriod {GroupName = "Period #1", Start = new DateTime(2020,03,13), End = new DateTime(2021,03,13), NumberOfDays=364},
+                new ManagementPeriod {GroupName = "Period #2", Start = new DateTime(2022,12,12), End = new DateTime(2023,12,12), NumberOfDays=364}
             };
         }
 
--- H.GUI.Avalonia/H.Avalonia/ViewModels/ComponentViews/OtherAnimals/HorsesComponentViewModelDesign.cs
+++ H.GUI.Avalonia/H.Avalonia/ViewModels/ComponentViews/OtherAnimals/HorsesComponentViewModelDesign.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using H.Core.Models.Animals;
+using H.Core.Enumerations;
 
 namespace H.Avalonia.ViewModels.ComponentViews.OtherAnimals
 {
@@ -8,7 +9,8 @@ namespace H.Avalonia.ViewModels.ComponentViews.OtherAnimals
         public HorsesComponentViewModelDesign() 
         {
             base.ViewName = "Horses";
-            base.ManagementPeriods.Add(new ManagementPeriod { GroupName = "Test Group #1", Start=new DateTime(2000,01,01), End=new DateTime(2001,01,01), NumberOfDays=364});
+            base.Groups.Add(new AnimalGroup { GroupType = AnimalType.Horses });
+            base.ManagementPeriods.Add(new ManagementPeriod { GroupName = "Test Group #1", Start = new DateTime(2000,01,01), End = new DateTime(2001,01,01), NumberOfDays=364});
         }
     }
 }

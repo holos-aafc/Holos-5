c7488d281c4e5ce79141b238ac3dade0ba4433a3
raheemmir <raheemmir15@gmail.com>
2025-02-07 15:53:54 -0700

    Factored out what's currently there for step 3 (from horse view)

---
+++ H.GUI.Avalonia/H.Avalonia/Views/ComponentViews/OtherAnimals/OtherAnimalsStep3View.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace H.Avalonia;
+
+public partial class OtherAnimalsStep3View : UserControl
+{
+    public OtherAnimalsStep3View()
+    {
+        InitializeComponent();
+    }
+}

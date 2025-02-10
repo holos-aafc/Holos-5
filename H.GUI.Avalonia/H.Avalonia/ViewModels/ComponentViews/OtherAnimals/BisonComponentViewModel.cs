7e1a226103a8bfa5b13f26518e1210418facc285
raheemmir <raheemmir15@gmail.com>
2025-02-10 15:03:08 -0700

    Added button functionality to horse and bison views / view models

--- H.GUI.Avalonia/H.Avalonia/ViewModels/ComponentViews/OtherAnimals/BisonComponentViewModel.cs
+++ H.GUI.Avalonia/H.Avalonia/ViewModels/ComponentViews/OtherAnimals/BisonComponentViewModel.cs
@@ -2,29 +2,63 @@
 using H.Core.Models.Animals;
 using System.Collections.ObjectModel;
 using System;
+using Prism.Regions;
 
 namespace H.Avalonia.ViewModels.ComponentViews.OtherAnimals
 {
     public class BisonComponentViewModel : ViewModelBase
     {
-        public string ViewName { get; set; }
-        public ObservableCollection<ManagementPeriod> ManagementPeriods { get; set; }
-        public ObservableCollection<AnimalGroup> Groups { get; set; }
+        #region Fields
+
+        private ObservableCollection<ManagementPeriod> _managementPeriods;
+        private ObservableCollection<AnimalGroup> _animalGroups;
+
+        #endregion
+
+        #region Constructors
         public BisonComponentViewModel()
         {
             ViewName = "Bison";
-
-            //populating dummy data
-            Groups = new ObservableCollection<AnimalGroup>
-            {
-                new AnimalGroup {GroupType = AnimalType.Bison}
-            };
-
-            ManagementPeriods = new ObservableCollection<ManagementPeriod>
-            {
-                new ManagementPeriod {GroupName = "Period #1", Start = new DateTime(2020,03,13), End = new DateTime(2021,03,13), NumberOfDays=364},
-                new ManagementPeriod {GroupName = "Period #2", Start = new DateTime(2022,12,12), End = new DateTime(2023,12,12), NumberOfDays=364}
-            };
+            Groups = new ObservableCollection<AnimalGroup>();
+            ManagementPeriods = new ObservableCollection<ManagementPeriod>();
         }
+
+        #endregion
+
+        #region Properties
+
+        public string ViewName { get; set; }
+
+        public ObservableCollection<ManagementPeriod> ManagementPeriods
+        {
+            get => _managementPeriods;
+            set => SetProperty(ref _managementPeriods, value);
+        }
+        public ObservableCollection<AnimalGroup> Groups
+        {
+            get => _animalGroups;
+            set => SetProperty(ref _animalGroups, value);
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public override void OnNavigatedTo(NavigationContext navigationContext)
+        {
+            base.OnNavigatedTo(navigationContext);
+        }
+
+        public void HandleAddGroupEvent()
+        {
+            Groups.Add(new AnimalGroup { GroupType = AnimalType.Bison });
+        }
+
+        public void HandleAddManagementPeriodEvent()
+        {
+            int numPeriods = ManagementPeriods.Count;
+            ManagementPeriods.Add(new ManagementPeriod { GroupName = $"Period #{numPeriods}", Start = new DateTime(2020, 03, 13), End = new DateTime(2021, 03, 13), NumberOfDays = 364 });
+        }
+        #endregion
     }
 }
--- H.GUI.Avalonia/H.Avalonia/ViewModels/ComponentViews/OtherAnimals/HorsesComponentViewModel.cs
+++ H.GUI.Avalonia/H.Avalonia/ViewModels/ComponentViews/OtherAnimals/HorsesComponentViewModel.cs
@@ -8,32 +8,57 @@ namespace H.Avalonia.ViewModels.ComponentViews.OtherAnimals
 {
     public class HorsesComponentViewModel : ViewModelBase
     {
-        public string ViewName { get; set; }
+        #region Fields
 
-        public ObservableCollection<ManagementPeriod> ManagementPeriods { get; set; }
-        public ObservableCollection<AnimalGroup> Groups { get; set; }
+        private ObservableCollection<ManagementPeriod> _managementPeriods;
+        private ObservableCollection<AnimalGroup> _animalGroups;
 
+        #endregion
+
+        #region Constructors
         public HorsesComponentViewModel() 
         {
             ViewName = "Horses";
-            
-            // Populating some dummy data (i.e. for testing DataGrids)
-
-            Groups = new ObservableCollection<AnimalGroup>
-            {
-                new AnimalGroup {GroupType = AnimalType.Horses}
-            };
-
-            ManagementPeriods = new ObservableCollection<ManagementPeriod>
-            {
-                new ManagementPeriod {GroupName = "Period #1", Start = new DateTime(2020,03,13), End = new DateTime(2021,03,13), NumberOfDays=364},
-                new ManagementPeriod {GroupName = "Period #2", Start = new DateTime(2022,12,12), End = new DateTime(2023,12,12), NumberOfDays=364}
-            };
+            Groups = new ObservableCollection<AnimalGroup>();
+            ManagementPeriods = new ObservableCollection<ManagementPeriod>();
         }
+
+        #endregion
+
+        #region Properties
+
+        public string ViewName { get; set; }
+
+        public ObservableCollection<ManagementPeriod> ManagementPeriods
+        {
+            get => _managementPeriods;
+            set => SetProperty(ref _managementPeriods, value);
+        }
+        public ObservableCollection<AnimalGroup> Groups
+        {
+            get => _animalGroups;
+            set => SetProperty(ref _animalGroups, value);
+        }
+
+        #endregion
+
+        #region Public Methods
 
         public override void OnNavigatedTo(NavigationContext navigationContext)
         {
             base.OnNavigatedTo(navigationContext);
         }
+
+        public void HandleAddGroupEvent()
+        {
+            Groups.Add(new AnimalGroup { GroupType = AnimalType.Horses });
+        }
+
+        public void HandleAddManagementPeriodEvent()
+        {
+            int numPeriods = ManagementPeriods.Count;
+            ManagementPeriods.Add(new ManagementPeriod { GroupName = $"Period #{numPeriods}", Start = new DateTime(2020, 03, 13), End = new DateTime(2021, 03, 13), NumberOfDays = 364 });
+        }
+        #endregion
     }
 }

c30b63dfc0d59e954cd3289e05e1c37bfcd54e95
Panchal, Zishan (AAFC AAC) <zishan.panchal@agr.gc.ca>
2025-02-10 14:15:06 -0700

    Made changes to Bison component View, ViewModel and ViewModelDesign using the factored OtherAnimalViews

--- H.GUI.Avalonia/H.Avalonia/ViewModels/ComponentViews/OtherAnimals/BisonComponentViewModel.cs
+++ H.GUI.Avalonia/H.Avalonia/ViewModels/ComponentViews/OtherAnimals/BisonComponentViewModel.cs
@@ -1,7 +1,30 @@
-﻿namespace H.Avalonia.ViewModels.ComponentViews.OtherAnimals
+﻿using H.Core.Enumerations;
+using H.Core.Models.Animals;
+using System.Collections.ObjectModel;
+using System;
+
+namespace H.Avalonia.ViewModels.ComponentViews.OtherAnimals
 {
     public class BisonComponentViewModel : ViewModelBase
     {
+        public string ViewName { get; set; }
+        public ObservableCollection<ManagementPeriod> ManagementPeriods { get; set; }
+        public ObservableCollection<AnimalGroup> Groups { get; set; }
+        public BisonComponentViewModel()
+        {
+            ViewName = "Bison";
 
+            //populating dummy data
+            Groups = new ObservableCollection<AnimalGroup>
+            {
+                new AnimalGroup {GroupType = AnimalType.Bison}
+            };
+
+            ManagementPeriods = new ObservableCollection<ManagementPeriod>
+            {
+                new ManagementPeriod {GroupName = "Period #1", Start = new DateTime(2020,03,13), End = new DateTime(2021,03,13), NumberOfDays=364},
+                new ManagementPeriod {GroupName = "Period #2", Start = new DateTime(2022,12,12), End = new DateTime(2023,12,12), NumberOfDays=364}
+            };
+        }
     }
 }
---
+++ H.GUI.Avalonia/H.Avalonia/ViewModels/ComponentViews/OtherAnimals/BisonComponentViewModelDesign.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using H.Core.Enumerations;
+using H.Core.Models.Animals;
+
+namespace H.Avalonia.ViewModels.ComponentViews.OtherAnimals
+{
+    public class BisonComponentViewModelDesign : BisonComponentViewModel
+    {
+        public BisonComponentViewModelDesign() {
+            base.ViewName = "Bison";
+            base.Groups.Add(new AnimalGroup { GroupType = AnimalType.Bison });
+            base.ManagementPeriods.Add(new ManagementPeriod { GroupName = "Test Group #1", Start = new DateTime(2000, 01, 01), End = new DateTime(2001, 01, 01), NumberOfDays = 364 });
+        }
+    }
+}

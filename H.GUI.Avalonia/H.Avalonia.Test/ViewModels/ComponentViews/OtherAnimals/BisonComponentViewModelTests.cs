210bae916638afa29f7ff718ba72a331bd8f4196
raheemmir <raheemmir15@gmail.com>
2025-02-14 10:29:44 -0700

    Added basic unit tests for the other other animals view models child classes

---
+++ H.GUI.Avalonia/H.Avalonia.Test/ViewModels/ComponentViews/OtherAnimals/BisonComponentViewModelTests.cs
@@ -0,0 +1,35 @@
+﻿using H.Core.Enumerations;
+
+namespace H.Avalonia.ViewModels.ComponentViews.OtherAnimals.Tests
+{
+    [TestClass]
+    public class BisonComponentViewModelTests
+    {
+        private static BisonComponentViewModel _viewModel;
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext context)
+        {
+            _viewModel = new BisonComponentViewModel();
+        }
+
+        [ClassCleanup]
+        public static void ClassCleanup()
+        {
+        }
+
+        [TestMethod]
+        public void TestConstructorSettingViewName()
+        {
+            string expectedName = "Bison";
+            Assert.AreEqual(expectedName, _viewModel.ViewName);
+        }
+
+        [TestMethod]
+        public void TestConstructorSettingAnimalType()
+        {
+            AnimalType expectedAnimalType = AnimalType.Bison;
+            Assert.AreEqual(expectedAnimalType, _viewModel.OtherAnimalType);
+        }
+    }
+}
---
+++ H.GUI.Avalonia/H.Avalonia.Test/ViewModels/ComponentViews/OtherAnimals/DeerComponentViewModelTests.cs
@@ -0,0 +1,35 @@
+﻿using H.Core.Enumerations;
+
+namespace H.Avalonia.ViewModels.ComponentViews.OtherAnimals.Tests
+{
+    [TestClass]
+    public class DeerComponentViewModelTests
+    {
+        private static DeerComponentViewModel _viewModel;
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext context)
+        {
+            _viewModel = new DeerComponentViewModel();
+        }
+
+        [ClassCleanup]
+        public static void ClassCleanup()
+        {
+        }
+
+        [TestMethod]
+        public void TestConstructorSettingViewName()
+        {
+            string expectedName = "Deer";
+            Assert.AreEqual(expectedName, _viewModel.ViewName);
+        }
+
+        [TestMethod]
+        public void TestConstructorSettingAnimalType()
+        {
+            AnimalType expectedAnimalType = AnimalType.Deer;
+            Assert.AreEqual(expectedAnimalType, _viewModel.OtherAnimalType);
+        }
+    }
+}

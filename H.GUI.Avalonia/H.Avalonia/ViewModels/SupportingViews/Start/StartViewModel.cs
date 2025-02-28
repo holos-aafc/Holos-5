3e3f686fe559864caa1ca3bf9cd45939e226e87e
Bristow, Wyatt (AAFC AAC) <wyatt.bristow@agr.gc.ca>
2025-02-28 12:06:02 -0700

    Created StartViewModel

---
+++ H.GUI.Avalonia/H.Avalonia/ViewModels/SupportingViews/Start/StartViewModel.cs
@@ -0,0 +1,68 @@
+﻿using Prism.Events;
+using Prism.Regions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H.Avalonia.ViewModels.SupportingViews.Start
+{
+    class StartViewModel : ViewModelBase
+    {
+        #region Fields
+
+        private bool _isBusy;
+        private string _IsBusyMessage;
+        private int _progressValue;
+
+        #endregion
+
+        #region Constructors
+
+        public StartViewModel()
+        {
+
+        }
+
+        public StartViewModel(IRegionManager regionManager,
+                              IEventAggregator eventAggregator,
+                              Storage storageService) : base(regionManager, eventAggregator, storageService)
+        {
+
+        }
+
+        #endregion
+
+        #region Properties
+
+        public bool IsBusy
+        {
+            get { return _isBusy; }
+            set
+            {
+                this.SetProperty(ref _isBusy, value, () =>
+                {
+                    if (_isBusy)
+                    {
+                    }
+                });
+            }
+        }
+
+        public string IsBusyMessage
+        {
+            get { return _IsBusyMessage; }
+            set { this.SetProperty(ref _IsBusyMessage, value); }
+        }
+
+        public int ProgressValue
+        {
+            get { return _progressValue; }
+            set { this.SetProperty(ref _progressValue, value); }
+        }
+
+        #endregion
+
+    }
+}

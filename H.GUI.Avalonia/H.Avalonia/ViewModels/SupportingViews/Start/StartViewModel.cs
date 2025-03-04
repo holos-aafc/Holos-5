c364877d25fa70827cc8245c16ecd517e48a65a9
raheemmir <raheem.mir@agr.gc.ca>
2025-03-04 09:04:33 -0700

    Cleaned up StartViewModel

--- H.GUI.Avalonia/H.Avalonia/ViewModels/SupportingViews/Start/StartViewModel.cs
+++ H.GUI.Avalonia/H.Avalonia/ViewModels/SupportingViews/Start/StartViewModel.cs
@@ -1,21 +1,16 @@
-﻿using H.Avalonia.ViewModels.SupportingViews.Disclaimer;
-using H.Core.Properties;
+﻿using H.Core.Properties;
 using H.Core.Providers;
 using Prism.Events;
 using Prism.Regions;
 using System;
-using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 using H.Infrastructure;
 using Avalonia;
 using Avalonia.Threading;
 using H.Avalonia.Views.FarmCreationViews;
 using H.Core.Services.StorageService;
-using System.Diagnostics;
 
 namespace H.Avalonia.ViewModels.SupportingViews.Start
 {
@@ -27,11 +22,7 @@ namespace H.Avalonia.ViewModels.SupportingViews.Start
         private string _IsBusyMessage;
         private int _progressValue;
 
-        IGeographicDataProvider _geographicDataProvider;
-        DisclaimerViewModel _disclaimerViewModel;
-        FarmOptionsViewModel _farmOptionsViewModel;
-        FarmCreationViewModel _farmCreationViewModel;
-        IStorageService _storageService;
+        GeographicDataProvider _geographicDataProvider;
 
         #endregion
 
@@ -41,10 +32,17 @@ namespace H.Avalonia.ViewModels.SupportingViews.Start
         {
 
         }
-        // IGeographicDataProvider geographicDataProvider,
-        public StartViewModel(IRegionManager regionManager, IEventAggregator eventAggregator, IStorageService storageService, FarmCreationViewModel farmCreationViewModel, GeographicDataProvider geographicDataProvider) : base(regionManager, eventAggregator, storageService) 
+ 
+        public StartViewModel(IRegionManager regionManager, IEventAggregator eventAggregator, IStorageService storageService, GeographicDataProvider geographicDataProvider) : base(regionManager, eventAggregator, storageService) 
         {
-
+            if (geographicDataProvider != null)
+            {
+                _geographicDataProvider = geographicDataProvider;
+            }
+            else
+            {
+                throw (new ArgumentNullException(nameof(geographicDataProvider)));
+            }
         }
 
         #endregion
@@ -107,24 +105,21 @@ namespace H.Avalonia.ViewModels.SupportingViews.Start
 
         private void OnBackgroundWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
         {
-            Debug.WriteLine("Top of WorkerCompleted");
             if (sender is BackgroundWorker backgroundWorker)
             {
                 backgroundWorker.DoWork -= this.OnBackgroundWorkerDoWork;
                 backgroundWorker.RunWorkerCompleted -= this.OnBackgroundWorkerCompleted;
                 this.InvokeOnUiThread(() => { this.IsBusy = false; });
 
-                // When first installed, user will have no farms. Show the new farm dialog.
-                if (StorageService.GetAllFarms().Count > 0 == false)
+                // When first installed, user will have no farms. Show the create new farm view.
+                if (StorageService.GetAllFarms().Any() == false)
                 {
-                    //_farmCreationViewModel.OnCreateNewFarmExecute();
+                    base.RegionManager.RequestNavigate(UiRegions.ContentRegion, nameof(FarmCreationView));
                 }
                 else
                 {
-                    Debug.WriteLine("else block in WorkerCompleted");
-                    // If this is not the first run after installation (there is at least one farm), show the landing view.
+                    // If this is not the first run after installation (there is at least one farm), show the farm options view.
                     base.RegionManager.RequestNavigate(UiRegions.ContentRegion, nameof(FarmOptionsView));
-                    Debug.WriteLine("After ReigonManager Call");
                 }
             }
         }
@@ -133,40 +128,33 @@ namespace H.Avalonia.ViewModels.SupportingViews.Start
         {
             try
             {
-                // Any exceptions here will not be caught by Application.DispatcherUnhandledException...
-
                 // This is running on a separate thread than the main UI thread, need to set culture for this thread
-                //if (Settings.Default.DisplayLanguage.Equals(H.Core.Enumerations.Languages.French.GetDescription(), StringComparison.InvariantCultureIgnoreCase))
-                //{
-                //    var culture = InfrastructureConstants.FrenchCultureInfo;
-                //    Thread.CurrentThread.CurrentCulture = culture;
-                //    Thread.CurrentThread.CurrentUICulture = culture;
-                //}
+                if (Settings.Default.DisplayLanguage.Equals(H.Core.Enumerations.Languages.French.GetDescription(), StringComparison.InvariantCultureIgnoreCase))
+                {
+                    var culture = H.Infrastructure.InfrastructureConstants.FrenchCultureInfo;
+                    Thread.CurrentThread.CurrentCulture = culture;
+                    Thread.CurrentThread.CurrentUICulture = culture;
+                }
 
-                this.IsBusyMessage = H.Core.Properties.Resources.MessageLoadingPleaseWait; // make this a resource
-                Debug.WriteLine("Before setting progress value");
+                this.IsBusyMessage = H.Core.Properties.Resources.MessageLoadingPleaseWait;
+
                 this.ProgressValue = 0;
-                Debug.WriteLine("After setting 0");
                 Thread.Sleep(2000);
                 this.ProgressValue = 25;
-                Debug.WriteLine("After setting 25");
-                Thread.Sleep(2000);
-                //_geographicDataProvider.Initialize();
+
+                _geographicDataProvider.Initialize();
 
                 this.ProgressValue = 50;
-                Debug.WriteLine("After setting 50");
                 Thread.Sleep(2000);
+
                 //base.InvokeOnUiThread(() => _mapViewModel.LoadMapFrameworkElements());
+                
                 this.ProgressValue = 75;
-                Debug.WriteLine("After setting 75");
                 Thread.Sleep(2000);
                 this.ProgressValue = 100;
-                Debug.WriteLine("After setting 100");
             }
             catch (Exception ex)
             {
-                // ... but we can handle it in the throwing thread and pass it to the main thread where Application.DispatcherUnhandledException can handle it
-
                 Dispatcher.UIThread.Invoke(new Action(() =>
                 {
                     throw new Exception($"Exception in {nameof(StartViewModel)}.{nameof(StartViewModel.OnBackgroundWorkerDoWork)}. Error: {ex.Message}", ex);

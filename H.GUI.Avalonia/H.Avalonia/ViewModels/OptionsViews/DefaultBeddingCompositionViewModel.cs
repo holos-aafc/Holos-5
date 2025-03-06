854562be5a9dfed3c1e051be1658bde1429423d6
raheemmir <raheem.mir@agr.gc.ca>
2025-03-06 14:56:39 -0700

    Added a Table_30_Default_Bedding...ViewModel for data validation

--- H.GUI.Avalonia/H.Avalonia/ViewModels/OptionsViews/DefaultBeddingCompositionViewModel.cs
+++ H.GUI.Avalonia/H.Avalonia/ViewModels/OptionsViews/DefaultBeddingCompositionViewModel.cs
@@ -1,4 +1,5 @@
-﻿using H.Core.Services.StorageService;
+﻿using System.Collections.ObjectModel;
+using H.Core.Services.StorageService;
 using Prism.Events;
 using Prism.Regions;
 
@@ -6,11 +7,38 @@ namespace H.Avalonia.ViewModels.OptionsViews
 {
     public class DefaultBeddingCompositionViewModel : ViewModelBase
     {
+        #region Fields
+
+        private ObservableCollection<Table_30_Default_Bedding_Material_Composition_ViewModel> _beddingMaterialCompositionTable30ViewModels;
+
+        #endregion
+
+        #region Constructors
+
         public DefaultBeddingCompositionViewModel(
             IRegionManager regionManager,
             IEventAggregator eventAggregator,
             IStorageService storageService) : base(regionManager, eventAggregator, storageService)
         {
+            BeddingMaterialCompositionTable30ViewModels = new ObservableCollection<Table_30_Default_Bedding_Material_Composition_ViewModel>();
+
+            foreach (var dataClassInstance in base.ActiveFarm.DefaultsCompositionOfBeddingMaterials)
+            {
+                var table30DefaultBeddingViewModel = new Table_30_Default_Bedding_Material_Composition_ViewModel(dataClassInstance);
+                BeddingMaterialCompositionTable30ViewModels.Add(table30DefaultBeddingViewModel);
+            }
         }
+
+        #endregion
+
+        #region Properties
+
+        public ObservableCollection<Table_30_Default_Bedding_Material_Composition_ViewModel> BeddingMaterialCompositionTable30ViewModels
+        {
+            get => _beddingMaterialCompositionTable30ViewModels;
+            set => SetProperty(ref _beddingMaterialCompositionTable30ViewModels, value);
+        }
+
+        #endregion
     }
 }
---
+++ H.GUI.Avalonia/H.Avalonia/ViewModels/OptionsViews/Table_30_Default_Bedding_Material_Composition_ViewModel.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using H.Core.Providers.Animals;
+
+namespace H.Avalonia.ViewModels.OptionsViews
+{
+    public class Table_30_Default_Bedding_Material_Composition_ViewModel : ViewModelBase
+    {
+        #region Fields
+        
+        private Table_30_Default_Bedding_Material_Composition_Data _dataClassInstance;
+        private double _totalNitrogenKilogramsDryMatter;
+        private double _totalPhosphorusKilogramsDryMatter;
+        private double _totalCarbonKilogramsDryMatter;
+        private double _carbonToNitrogenRatio;
+
+        #endregion
+
+        #region Constructors
+        /// <summary>
+        /// This class is a view model wrapper over <see cref="Table_30_Default_Bedding_Material_Composition_Data"/> that provides data validation 
+        /// </summary>
+        /// <param name="dataClassInstance"></param>
+        public Table_30_Default_Bedding_Material_Composition_ViewModel(Table_30_Default_Bedding_Material_Composition_Data dataClassInstance)
+        {
+            if (dataClassInstance != null)
+            {
+                _dataClassInstance = dataClassInstance;
+
+                _totalNitrogenKilogramsDryMatter = _dataClassInstance.TotalNitrogenKilogramsDryMatter;
+                _totalPhosphorusKilogramsDryMatter = _dataClassInstance.TotalPhosphorusKilogramsDryMatter;
+                _totalCarbonKilogramsDryMatter = _dataClassInstance.TotalCarbonKilogramsDryMatter;
+                _carbonToNitrogenRatio = _dataClassInstance.CarbonToNitrogenRatio;
+            }
+            else
+            {
+                throw (new ArgumentNullException(nameof(dataClassInstance)));
+            }
+        }
+
+        #endregion
+
+        #region Properties
+
+        public int MyProperty { get; set; }
+
+        public string ComponentCategoryString
+        {
+            get { return _dataClassInstance.ComponentCategoryString; }
+        }
+
+        public string BeddingMaterialString
+        {
+            get { return _dataClassInstance.BeddingMaterialString; }
+        }
+
+        public double TotalNitrogenKilogramsDryMatter
+        {
+            get => _totalNitrogenKilogramsDryMatter;
+            set
+            {
+                if(SetProperty(ref _totalNitrogenKilogramsDryMatter, value))
+                {
+                    if(ValidateNumericProperty(nameof(TotalNitrogenKilogramsDryMatter), value))
+                    {
+                        _dataClassInstance.TotalNitrogenKilogramsDryMatter = value;
+                    }
+                }
+            }
+        }
+
+        public double TotalPhosphorusKilogramsDryMatter
+        {
+            get => _totalPhosphorusKilogramsDryMatter;
+            set
+            {
+                if (SetProperty(ref _totalPhosphorusKilogramsDryMatter, value))
+                {
+                    if(ValidateNumericProperty(nameof(TotalPhosphorusKilogramsDryMatter), value))
+                    {
+                        _dataClassInstance.TotalPhosphorusKilogramsDryMatter = value;
+                    }
+                }
+            }
+                
+        }
+
+        public double TotalCarbonKilogramsDryMatter
+        {
+            get => _totalCarbonKilogramsDryMatter;
+            set
+            {
+                if(SetProperty(ref _totalCarbonKilogramsDryMatter, value))
+                {
+                    if(ValidateNumericProperty(nameof(TotalCarbonKilogramsDryMatter), value))
+                    {
+                        _dataClassInstance.TotalCarbonKilogramsDryMatter = value;
+                    }
+                }
+            }
+        }
+
+        public double CarbonToNitrogenRatio
+        {
+            get => _carbonToNitrogenRatio;
+            set
+            {
+                if (SetProperty(ref _carbonToNitrogenRatio, value))
+                {
+                    if (ValidateNumericProperty(nameof(CarbonToNitrogenRatio), value))
+                    {
+                        _dataClassInstance.CarbonToNitrogenRatio = value;
+                    }
+                }
+            }
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private bool ValidateNumericProperty(string propertyName, double property)
+        {
+            RemoveError(propertyName);
+
+            if (property < 0.0)
+            {
+                AddError(propertyName, H.Core.Properties.Resources.ErrorMustBeGreaterThan0);
+                return false;
+            }
+            
+            return true;
+        }
+
+        #endregion
+    }
+}

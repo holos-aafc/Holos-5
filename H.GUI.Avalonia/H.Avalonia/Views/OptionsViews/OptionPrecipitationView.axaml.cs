e46e650894a24f46430b60da249f7dd1865f25ef
Bristow, Wyatt (AAFC AAC) <wyatt.bristow@agr.gc.ca>
2025-03-20 09:46:03 -0600

    Chart in PrecipitationView now updates with input VM values

--- H.GUI.Avalonia/H.Avalonia/Views/OptionsViews/OptionPrecipitationView.axaml.cs
+++ H.GUI.Avalonia/H.Avalonia/Views/OptionsViews/OptionPrecipitationView.axaml.cs
@@ -1,68 +1,89 @@
 using Avalonia;
 using Avalonia.Controls;
-using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
-using H.Avalonia.ViewModels.OptionsViews;
-using H.Avalonia.ViewModels.SupportingViews.Disclaimer;
-using H.Core.Services.StorageService;
 using LiveChartsCore.SkiaSharpView.Painting;
 using LiveChartsCore.SkiaSharpView;
 using LiveChartsCore;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
 using SkiaSharp;
-using System.ComponentModel;
-using H.Avalonia.ViewModels;
+using H.Avalonia.ViewModels.OptionsViews;
+using H.Core.Providers.Temperature;
+using H.Core.Enumerations;
+using System.Collections.ObjectModel;
+using System;
 
 namespace H.Avalonia;
 
 public partial class OptionPrecipitationView : UserControl
 {
+    #region Fields
+
     private OptionPrecipitationViewModel _viewModel;
-    
+
+    public OptionPrecipitationView? ViewModel => DataContext as OptionPrecipitationView;
+
+    #endregion
+
+    #region Constructors
+
     public OptionPrecipitationView(OptionPrecipitationViewModel viewModel)
     {
         InitializeComponent();
         this._viewModel = viewModel;
-        this._viewModel.Data.PropertyChanged += OnDataPropertyChanged;
+        viewModel.BindingPrecipitationData.PropertyChanged += ViewModel_PropertyChanged;
         BuildChart();
     }
-    
 
-    private void OnDataPropertyChanged(object? sender, System.ComponentModel.PropertyChangedEventArgs e)
-    {
-        BuildChart();
-    }
+    #endregion
     
     private void BuildChart()
     {
         PrecipitationChart.Series = Series;
         PrecipitationChart.XAxes = XAxes;
-        Series[0].Values = this._viewModel.Data.PrecipitationSeriesValues;
+
+        var values = new ObservableCollection<double> { };
+        foreach (Months month in Enum.GetValues(typeof(Months)))
+        {
+            {
+                values.Add(Math.Round(this._viewModel.BindingPrecipitationData.GetValueByMonth(month), 2));
+            };
+        }
+        Series[0].Values = values;
     }
+
     public ISeries[] Series { get; set; } =
     {
         new ColumnSeries<double>
         {
             Values = new double[] {},
             Fill = new SolidColorPaint(SKColors.DarkSeaGreen),
+            
         }
     };
 
-        public Axis[] XAxes { get; set; }
-            = new Axis[]
+    public Axis[] XAxes { get; set; }
+        = new Axis[]
+    {
+        new Axis
         {
-            new Axis
-            {
-                Name = "Months",
-                NamePaint = new SolidColorPaint(SKColors.Black),
+            Name = "Months",
+            NamePaint = new SolidColorPaint(SKColors.Black),
 
-                LabelsPaint = new SolidColorPaint(SKColors.Black),
-                TextSize = 14,
-                LabelsRotation = 20,
-                ShowSeparatorLines = true,
+            LabelsPaint = new SolidColorPaint(SKColors.Black),
+            TextSize = 14,
+            LabelsRotation = 20,
+            ShowSeparatorLines = true,
 
-                Labels = new string[] { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"}
-            }
-        };
+            Labels = new string[] { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"}
+        }
+    };
+
+    #region Event Handlers
+
+    private void ViewModel_PropertyChanged(object? sender, System.ComponentModel.PropertyChangedEventArgs e)
+    {
+        BuildChart();
+    }
+
+    #endregion
 
 }

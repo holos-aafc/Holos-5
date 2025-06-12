6ce8a60d2cea141f445d114d96f73705635f45d6
Adnan Shafi <112738018+adnanshafi99@users.noreply.github.com>
2025-06-12 12:15:26 -0500

    Fertilizer Tab
    -Adding content for Fertilizer Tab (Partially Done)

--- H.GUI.Avalonia/H.Avalonia/ViewModels/ComponentViews/Crop/CropComponentViewModel.cs
+++ H.GUI.Avalonia/H.Avalonia/ViewModels/ComponentViews/Crop/CropComponentViewModel.cs
@@ -1,8 +1,33 @@
+using System.ComponentModel;
+
 namespace H.Avalonia.ViewModels.ComponentViews.Crop
 
 {
-    public class CropComponentViewModel : ViewModelBase
+    public class CropComponentViewModel : INotifyPropertyChanged
     {
+        private bool _showAdditionalInformation;
+        public bool ShowAdditionalInformation
+        {
+            get => _showAdditionalInformation;
+            set
+            {
+                if (_showAdditionalInformation != value)
+                {
+                    _showAdditionalInformation = value;
+                    OnPropertyChanged(nameof(ShowAdditionalInformation));
+                    OnPropertyChanged(nameof(HideAdditionalInformation));
+                }
+            }
+        }
 
+        public bool HideAdditionalInformation
+        {
+            get => !ShowAdditionalInformation;
+            set => ShowAdditionalInformation = !value;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
 }

c4395c30028c046c5e68ca1e406bb2f830257964
Aaron McPherson <95769272+holos-aafc@users.noreply.github.com>
2025-06-12 09:57:02 -0600

    Add data grid for selected crops on a field component

--- H.GUI.Avalonia/H.Avalonia/ViewModels/ComponentViews/LandManagement/Field/FieldComponentViewModelDesign.cs
+++ H.GUI.Avalonia/H.Avalonia/ViewModels/ComponentViews/LandManagement/Field/FieldComponentViewModelDesign.cs
@@ -1,4 +1,6 @@
-﻿using H.Core.Factories;
+﻿using System.Collections.ObjectModel;
+using H.Core.Enumerations;
+using H.Core.Factories;
 using H.Core.Services.StorageService;
 using Prism.Events;
 using Prism.Regions;
@@ -11,6 +13,11 @@ public class FieldComponentViewModelDesign : FieldComponentViewModel
     {
         base.SelectedFieldSystemComponentDto = new FieldSystemComponentDto();
         base.SelectedFieldSystemComponentDto.Name = "A Field";
+
+        base.CropDtos = new ObservableCollection<ICropDto>()
+        {
+            new CropDto() { CropType = CropType.Wheat },
+        };
     }
 
     public FieldComponentViewModelDesign(IRegionManager regionManager, IEventAggregator eventAggregator, IStorageService storageService, IFieldComponentDtoFactory fieldComponentDtoFactory, ICropDtoFactory cropDtoFactory) : base(regionManager, eventAggregator, storageService, fieldComponentDtoFactory, cropDtoFactory)

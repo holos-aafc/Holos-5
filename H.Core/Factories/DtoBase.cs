2c3309135204f30a0c08b3939098f929c84bbaed
holos-aafc <95769272+holos-aafc@users.noreply.github.com>
2025-06-13 13:50:15 -0600

    Add comments

--- H.Core/Factories/DtoBase.cs
+++ H.Core/Factories/DtoBase.cs
@@ -3,6 +3,10 @@ using Prism.Mvvm;
 
 namespace H.Core.Factories;
 
+/// <summary>
+/// A base class to be used with any other classes that must validate user input. The properties in this class and subclasses are properties
+/// that are bound to GUI controls and should therefore be validated before passes on the input values to the domain/business class objects.
+/// </summary>
 public abstract class DtoBase : ErrorValidationBase
 {
     #region Fields

322cbf83e8f4d262715be360bb74a2bb8a580208
raheemmir <raheem.mir@agr.gc.ca>
2025-06-18 09:13:06 -0600

    Xaml refactoring for farms datagrid + working file -> close

--- H.GUI.Avalonia/H.Avalonia/Views/FarmCreationViews/FarmDataGridSharedView.axaml.cs
+++ H.GUI.Avalonia/H.Avalonia/Views/FarmCreationViews/FarmDataGridSharedView.axaml.cs
@@ -4,9 +4,9 @@ using Avalonia.Markup.Xaml;
 
 namespace H.Avalonia;
 
-public partial class FarmOpenSharedView : UserControl
+public partial class FarmDataGridSharedView : UserControl
 {
-    public FarmOpenSharedView()
+    public FarmDataGridSharedView()
     {
         InitializeComponent();
     }
